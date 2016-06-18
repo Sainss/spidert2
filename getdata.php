0bc4c24a78eb35715adf0cfd906a01887c81ba69
Sainss <sai.n.suprabhath@gmail.com>
2016-06-18 23:48:28 +0530

    Update getdata.php

--- getdata.php
+++ getdata.php
@@ -9,7 +9,17 @@ if(!empty($_POST)){
     die( 'Connect Error: ' . $conn->connect_errno . ': ' . $conn->connect_error );
   }
 
-
+$roll=$_POST['roll'];
+$query="SELECT * FROM info WHERE Rollno= $roll";
+$result=mysqli_query($conn,$query);
+if(mysqli_num_rows($result)>0)
+{
+  echo 'Roll number already exists.';
+  echo 'To go back to menu <a href="index.html">Click here</a>';
+}
+else 
+  {
+  $pass=rand(1000,9999);
   $sql = "INSERT INTO info(Name,Rollno,Department,Emailid,Physicaladd,Aboutme)
   VALUES( '{$conn->real_escape_string($_POST['Name'])}', '{$conn->real_escape_string($_POST['roll'])}', '{$conn->real_escape_string($_POST['Department'])}', '{$conn->real_escape_string($_POST['email'])}', '{$conn->real_escape_string($_POST['address'])}', '{$conn->real_escape_string($_POST['myself'])}')";
 
@@ -19,7 +29,7 @@ if(!empty($_POST)){
     } else {
     echo "Error: " . $sql . "<br>" . $conn->error;
  }
-
+}
  $conn->close();
 }
 
