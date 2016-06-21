16799bd71c58961aa88c6f2e1fa2725e7da62cdb
Sainss <sai.n.suprabhath@gmail.com>
2016-06-21 21:23:53 +0530

    Add files via upload

---
+++ fulldata.php
@@ -0,0 +1,49 @@
+
+<html>
+<head>
+
+</head>
+<body>
+<center><h1><u>Student Database</u></h1></center>
+
+
+<?php
+ $conn = new mysqli( 'localhost', 'root', '', 'student form' );
+  
+ if ( $conn->connect_error ) {
+    die( 'Connect Error: ' . $conn->connect_errno . ': ' . $conn->connect_error );
+  }
+
+$query="SELECT * FROM info ";
+$result=mysqli_query($conn,$query);
+while ($row = mysqli_fetch_array($result)){
+?>
+<table style=" border:1px solid silver" cellpadding="5px" cellspacing="0px"
+align="center" border="1">
+<tr>
+<td colspan="4" style="background:#0066FF; color:#FFFFFF; font-size:20px">VIEW
+STUDENT DATABASE</td></tr>
+<tr> 
+
+<td> Name</td><td><?php echo $row[1];?></td>
+<td> Roll Number</td><td><?php echo $row[2];?></td>
+</tr>
+<tr>
+<td> Department</td><td><?php echo $row[3];?></td>
+<td> Email Address</td><td><?php echo $row[4];?></td>
+</tr>
+<tr>
+<td>Physical Address</td><td><?php echo $row[5];?></td>
+<td>About me</td><td><?php echo $row[6];?></td></tr>
+<tr> 
+
+</table>
+<?php
+
+echo '<br>';
+}
+
+?>
+
+</body>
+</html> 
